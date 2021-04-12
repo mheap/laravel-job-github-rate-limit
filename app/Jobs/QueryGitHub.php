f2950680ab696dcf75c5d48a440358e6a5062ad4
Michael Heap <m@michaelheap.com>
2021-04-12 15:49:40 +0100

    Add job that calls the GitHub API

---
+++ app/Jobs/QueryGitHub.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Jobs;
+
+use GrahamCampbell\GitHub\Facades\GitHub;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class QueryGitHub implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $r = GitHub::me()->organizations();
+        dump($r);
+    }
+}
